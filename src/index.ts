a134b115a3fbb3b6d70b85b15989f4d951bdbcd5
Shota Iguchi <shota-iguchi@cookpad.com>
2023-09-25 01:39:11 +0900

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import * as core from '@actions/core';
+import * as github from '@actions/github';
+
+try {
+  const message = core.getInput('message');
+  console.log(message);
+  const time = (new Date()).toTimeString();
+  core.setOutput("time", time);
+  // Get the JSON webhook payload for the event that triggered the workflow
+  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  console.log(`The event payload: ${payload}`);
+} catch (error: any) {
+  if (error instanceof Error) core.setFailed(error.message)
+}
